ed2c0b8584b3fae6410412ac994f62309cf465e8
KonradBu <konradburgi1@gmail.com>
2024-09-03 23:11:57 +0200

    Add project files.

---
+++ MCClone/Game.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace MCClone
+{
+    internal class Game : GameWindow
+    {
+        float[] vertices = {
+            0f, 0.5f, 0f,
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f
+        };
+        // Render Pipeline
+        int vao;
+        int shaderProgram;
+
+
+
+        int Screenwidth;
+        int Screenheight;
+        public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            
+            this.Screenheight = height;
+            this.Screenwidth = width;
+            CenterWindow(new Vector2i(width, height));
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0,0,e.Width,e.Height);
+            this.Screenwidth = e.Width;
+            this.Screenheight= e.Height;
+        }
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            vao = GL.GenVertexArray();
+
+            int vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexArrayAttrib(vao, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+
+
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0.5f, 0.3f, 1f, 1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(args);
+
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        public static string LoadShaderSource(string filePath)
+        {
+            string shaderSource = "";
+            try
+            {
+                using (StreamReader reader = new StreamReader("../../../Shader/" + filePath))
+                {
+                    shaderSource = reader.ReadToEnd();
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to load shader source file " + e.Message);
+            }
+            return shaderSource;    
+        }
+    }
+}
---
+++ MCClone/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MCClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (game game = new game(500, 500)) {
+                game.Run();
+            }
+        }
+    }
+}
